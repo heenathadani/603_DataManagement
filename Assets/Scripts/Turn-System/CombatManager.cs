89b382e84150abd9a6b6c5851e31dce056d21e3e
Rin <yb6790@g.rit.edu>
2024-04-12 01:16:44 -0400

    Added body part and stats calculation

--- Assets/Scripts/Turn-System/CombatManager.cs
+++ Assets/Scripts/Turn-System/CombatManager.cs
@@ -9,6 +9,9 @@ public class CombatManager : MonoBehaviour
     private int _currentTurn;
     private CombatantType _activeType;
 
+    //Store the body part data list
+    public List<BodyPartData> _bodyPartDataList;
+
     public void DoAction()
     {
         if (_currentTurn == _activeCombatants.Count)
@@ -32,6 +35,15 @@ public class CombatManager : MonoBehaviour
         CombatantData.partyCharacters.Add(new Protagonist("Test Protagonist 1"));
         CombatantData.partyCharacters.Add(new Protagonist("Test Protagonist 2"));
 
+        // Temp, Check status update -- Rin
+        Protagonist protagonist = CombatantData.partyCharacters[0] as Protagonist;
+        if (protagonist != null && _bodyPartDataList.Count > 0)
+        {
+            protagonist.AddBodyPart(new BodyPart(_bodyPartDataList[0]));
+            protagonist.AddBodyPart(new BodyPart(_bodyPartDataList[0]));
+            protagonist.UpdateStatus();
+        }
+
         // Set up this controller
         _currentTurn = 0;
         _activeType = CombatantType.ALLIES;
@@ -39,6 +51,9 @@ public class CombatManager : MonoBehaviour
 
         Debug.Log("Data set up!");
     }
+
+
+
 
     private void OnEnable()
     {
