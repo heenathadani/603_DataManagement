4c32b822637108e46e639076c43e214902b7327c
Rin <yb6790@g.rit.edu>
2024-04-22 20:44:38 -0400

    player animation

--- Assets/Scripts/Exploration/PlayerMovement.cs
+++ Assets/Scripts/Exploration/PlayerMovement.cs
@@ -9,11 +9,19 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("horizontal", movement.x);
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("vertical", movement.y);
 
     }
 
