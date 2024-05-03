754dd9bc8641287f0d9b331c9914039dbd6d17ae
Heena Thadani <heenathadani26@gmail.com>
2024-05-02 22:17:29 -0400

    added backend to the stat scene

---
+++ Assets/Scripts/StatScene/PlayerSlot.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSlot : MonoBehaviour
+{
+    public GameObject actionOne;
+    public GameObject actionTwo;
+    public GameObject actionThree;
+    public GameObject actionFour;
+    public GameObject FillStrengthOne;
+    public GameObject FillStrengthTwo;
+    public GameObject FillStrengthThree;
+    public GameObject FillStrengthFour;
+    BodyPartType bodyPartType;
+    BodyPartStats stats;
+    public void SetCombatant(Combatant.aCombatant player)
+    {
+        // Set the name of the player in the slot
+        transform.Find("PlayerName").GetComponent<TMPro.TextMeshProUGUI>().text = player.Name;
+        actionOne.GetComponent<TMPro.TextMeshProUGUI>().text = BodyPartType.Head.ToString();
+        actionTwo.GetComponent<TMPro.TextMeshProUGUI>().text = BodyPartType.Arm.ToString();
+        actionThree.GetComponent<TMPro.TextMeshProUGUI>().text = BodyPartType.Leg.ToString();
+        actionFour.GetComponent<TMPro.TextMeshProUGUI>().text = BodyPartType.Body.ToString();
+        FillStrengthOne.GetComponent<Slider>().value = player._equipment[BodyPartType.Head].bodyPartStats.effectValue;
+        FillStrengthOne.GetComponent<Slider>().value = player._equipment[BodyPartType.Head].bodyPartStats.effectValue;
+        FillStrengthTwo.GetComponent<Slider>().value = player._equipment[BodyPartType.Head].bodyPartStats.effectValue;
+        FillStrengthThree.GetComponent<Slider>().value = player._equipment[BodyPartType.Head].bodyPartStats.effectValue;
+        FillStrengthFour.GetComponent<Slider>().value = player._equipment[BodyPartType.Head].bodyPartStats.effectValue;
+    }
+}
